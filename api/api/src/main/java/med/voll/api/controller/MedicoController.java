8c1ef84be692838ce6346d3456c3c0688a937dc8
Carlos Henrique <ichfs9108@gmail.com>
2023-12-14 23:57:14 -0300

    Atualizacao de Dados

--- api/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,8 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.MedicoRepository;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.repository.MedicoRepository;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -28,5 +29,12 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
         return repository.findAll(pageable).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 }
--- api/api/src/main/java/med/voll/api/repository/MedicoRepository.java
+++ api/api/src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,4 +1,4 @@
-package med.voll.api;
+package med.voll.api.repository;
 
 import med.voll.api.medico.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
