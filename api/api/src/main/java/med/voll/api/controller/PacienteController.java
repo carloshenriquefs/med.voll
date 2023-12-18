33f35c18577ca470cfd89a81486bf55b2495b937
Carlos Henrique <ichfs9108@gmail.com>
2023-12-18 00:10:44 -0300

    Add - Endpoint listar pacientes

---
+++ api/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,24 @@
+package med.voll.api.controller;
+
+import med.voll.api.repository.PacienteRepository;
+import med.voll.api.paciente.DadosListagemPaciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+}
